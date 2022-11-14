ed33979ffa8c9cdb5e88e59509813cd0dcbf087a
ShazilHussain <116807199+ShazilHussain@users.noreply.github.com>
2022-11-14 17:33:17 +0500

    shazil js assignment 4
    shazil js assignment 4

---
+++ app.js
@@ -0,0 +1,157 @@
+alert("Welcome to visual studio code ")
+
+
+alert(" hello \n Let's to start")
+    
+    
+alert("Hello! enjoy you coding")
+    
+alert("my google \n enter your passward")
+    
+alert("Hello...Ican run this mobile Application and development  my web brower concole")
+    
+alert("my favourite course")
+    
+
+var user_name ;
+var myFirst_name =("syed shazil hussain zaidi")
+    
+var this_massege = "my name is shazil hussain zaidi" 
+var browser = "hello world"
+    
+alert(browser)
+alert(myFirst_name)
+    
+var old ="17 years old"
+alert(old)
+    
+var development ="catified mobile application devalopment"
+alert(development)
+    
+var food ;
+alert("Pizza\n Pizz \n Piz \n Pi \n p")
+    
+var shazil_gmail = " shazilzaidi19@gmail.com"
+alert("my email is " + shazil_gmail)
+    
+var javascript_book2 = "“A smarter \n way to learn JavaScript”."
+alert("I am trying to learn from the book " + javascript_book2)
+    
+    
+document.write("yeah I can learn html content throught javascript ")
+    
+var figure ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(figure)
+
+
+alert("my age " + old)
+    
+var visiter = "“You have visited this site 12 times”."
+alert(visiter)
+    
+document.write(" my birth is 2005 data type of my declered variable is number ")
+    
+var visiterd_Name = "shazil hussain"
+var product_Name = "T-shirt"
+var quatity = "8 T shirt"
+    
+document.write(visiterd_Name + " ordered " + quatity +"(s) on XYZ Clothing store.")
+
+
+var name1 ="SHAZIL HUSSAIN"
+var namw2 ="SHAZIL HUSSAIN"
+var namw3 ="SHAZIL HUSSAIN"
+    
+var legalOne = "legal variable"
+var legal$ = "legal variable"
+var legal_three = "legal variable"
+var legal$four = "legal variable"
+var legal = "legal variable"
+    
+//var illegal-one ="illegal"
+//var illegal@two ="illagal"
+//var illegal&three ="illegal"
+//var illegal four ="illegal"
+//var 5phone ="illegal"
+    
+document.write(" <h1>Rules for naming JS variables</h1>")
+    
+var nameone = "$my_1stVariable" ;
+var name_two = "$ and" ;
+document.write(" Variable names can only contain ," + name_two + " For example " + nameone)
+    
+document.write(" Variables must begin with a or. For example $name, _name or name")
+var sensitve ="sensitive";
+document.write(" Variable names are case " + sensitve);
+var keyword ="keyword";
+document.write(" Variable names should not be JS " + keyword);
+
+
+var a = 3;
+var b = 5;
+var c = a + b;
+document.write(" Sums of " + " 3 " + " and " + " 5 " + " is " + c);
+
+var r = 2 + 2
+var u = 4 - 6
+var v = 5 * 9
+var g = 5 / 4
+
+var math ;
+document.write(" Value after variable declaration is: undifined ");
+
+var t = 1 + 1 + 1 + 1 + 1 + 7;
+document.write(++t + " ")
+
+var t ;
+document.write(--t + " ")
+
+
+var new3 = 600 * 5;
+document.write(" Total cost to buy ticket to a movie is " + new3 + " PKR <br> " );
+
+var table = parseInt(prompt("Enter your table value"))
+for(var s = 1 ; s <= 10 ; s++){
+    var resul = s*table
+    document.write(`&nbsp &nbsp ${table} X ${s} = ${resul}<br>`)
+}
+
+var  rupees1 = 650;
+var  rupees2 = 3;
+var  rupees3 = 100;
+var  rupees4 = 7;
+var  rupees5 = 100;
+document.write(" rupees1 = 650 <br> " + " rupees2 = 3<br " + " rupees3 = 100<br> " + " rupees4 = 7<br> " + " rupees5 = 100<br> ")
+
+var r = rupees1 + rupees2
+var j = rupees3 + rupees4
+var d = r + j + rupees5
+document.write(" Total cost of your is " + d)
+console.log(r)
+console.log(j)
+console.log(d)
+document.write(" <h1>Mark sheet</h1> ")
+
+var total = 980;
+var total_mar = 884;
+var total_cal = total / total_mar * 100;
+document.write(" total = 980<br> " + " total_mar = 884<br> " + total_cal )
+
+var dom = 6;
+var van = dom + 5 + 10 / 2;
+console.log(van)
+
+var hand = 2022
+var hand6 = 2005
+var hand7 = hand - hand6;
+console.log(hand7)
+
+
+
+
+
+
+
+
+
+
